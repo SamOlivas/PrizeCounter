036eff78e0a2ce2bb8a8cf8bfe645d144d004792
SamOlivas <sam.a.olivas@gmail.com>
2019-05-14 20:08:58 -0700

    link js and css

--- actions.js
+++ actions.js
@@ -1,1 +1,26 @@
-console.log('imported js')
+const data = {
+    customers: {
+      Moe: {
+        Foo: 0,
+        Bar: 0,
+        Bazz: 0
+      },
+      Larry: {
+        Foo: 0,
+        Bar: 0,
+        Bazz: 0
+      },
+      Curly: {
+        Foo: 0,
+        Bar: 0,
+        Bazz: 0
+      }
+    },
+    prizes: {
+      Foo: 1,
+      Bar: 3,
+      Bazz: 5
+    }
+  };
+
+  console.log(data)
